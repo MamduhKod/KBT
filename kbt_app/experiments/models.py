16835890e1304ebe7a2d17a9370cde8cd6d8d7cc
Mamduh Halawa <mamduhkod@gmail.com>
2024-06-20 13:06:29 +0200

    Redid models and reduced to two

--- kbt_app/experiments/models.py
+++ kbt_app/experiments/models.py
@@ -8,51 +8,35 @@ class ExperimentsConfig(AppConfig):
     name = 'experiments' 
     
     
-class Experiment(models.Model):
+class ExperimentPlan(models.Model):
     namn = models.CharField(max_length=50,)
+    Hypotes = models.CharField(max_length=100, 
+     help_text="Vad tror du kommer hända?")
+    negativ_tanke = models.CharField( max_length=50,
+        help_text="Vilken tanke vill du testa?")
     beskrivning = models.CharField(help_text="Beskriv syftet med ditt experiment.", max_length=300)
     datum = models.DateField(auto_now=False, auto_now_add=False, 
     help_text="När vill du utföra ditt experiment?")
-    def __str__(self):
-        return self.experiment_text
-    
-class Kognition(models.Model):
-    Experiment = models.ForeignKey(Experiment, on_delete=models.CASCADE)
-    negativ_tanke = models.CharField( max_length=50)
-    antagande = models.CharField( max_length=300)
-    
-    def __str__(self):
-        return self.kognition_text
-
-class ExperimentPlan(models.Model):
-    Experiment = models.ForeignKey(Experiment, on_delete=models.CASCADE)
     Beteende = models.CharField( max_length=100)
     Situation = models.DateField( max_length=100)
     Konsekvens = models.DateField(max_length=100)
-    
-    def __str__(self):
-        return self.experiment_plan_text
-class Hypotes(models.Model):
-    Experiment = models.ForeignKey(Experiment, on_delete=models.CASCADE)
-    hypotes = models.CharField(max_length=300)
-    tro_pre = models.SmallIntegerField()
-    
-    def __str__(self):
-        return self.hypotes_text
+    tro_pre = models.SmallIntegerField(help_text="Hur starkt tror du på din förutsägelse?")
 
-class Resultat(models.Model):
-    Experiment = models.ForeignKey(Experiment, on_delete=models.CASCADE)
-    resultat = models.CharField(max_length=300)
-    tro_post = models.SmallIntegerField()
+class ExperimentResultat(models.Model):
+    Experiment = models.ForeignKey(ExperimentPlan, on_delete=models.CASCADE)
+    resultat = models.CharField(max_length=300,help_text="Vad hände?")
+    insikter = models.CharField( max_length=300, help_text="Vilka insikter kom du till?")
+    tro_post = models.SmallIntegerField(help_text="Hur starkt tror du på din ursprungliga tanke nu?")
+    tro_pre_stored = models.SmallIntegerField(blank=True, null=True)
+    tro_post_skillnad = models.SmallIntegerField(blank=True, null=True)
     
+    @property
+    def tro_post_skillnad(self):
+       if self.tro_post and self.tro_pre_stored:
+           return self.tro_post - self.tro_pre_stored
+       else:
+           return None
+       
     def __str__(self):
         return self.results_text
-
-class Insikt(models.Model):
-    Experiment = models.ForeignKey(Experiment, on_delete=models.CASCADE)
-    insikter = models.CharField( max_length=300)
-    tro_skillnad = models.SmallIntegerField()
-    
-    def __str__(self):
-        return self.insikt_text
     
--- kbt_app/experiments/views.py
+++ kbt_app/experiments/views.py
@@ -5,26 +5,22 @@ from django.http import HttpResponse
 from django.urls import reverse_lazy
 
 # Create your views here.
-def Index(request):
-    experiment= Experiment.objects.all()
-    context = {'experiments': experiment}
-    return render(request,'experiments/index.html', context)
-
-def Detail(request):
-    experiment_detail = ExperimentPlan.objects.all()
-    context = {'experiment_detail': experiment_detail}
-    return render(request,'experiments/detail.html', context)
-
-
-class ExperimentCreateView(CreateView):
-    model = Experiment
-    fields = ["namn","beskrivning", "datum"]
-    success_url = reverse_lazy(('Index'))
+class ExperimentTankeView(CreateView):
+    model = ExperimentPlan
+    fields = ["negativ_tanke","Hypotes", "tro_pre"]
+    success_url = reverse_lazy(('Experiment'))
     
     def render_to_response(self, context, **kwargs):
         context['form'] = self.get_form()
         return super().render_to_response(context, **kwargs)
     
+
+#def Index(request):
+ #   experiment= ExperimentPlan.objects.all()
+  ## return render(request,'experiments/index.html', context)
+
+
+
     
 
     
