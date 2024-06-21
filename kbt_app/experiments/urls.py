a349319140cad21b04068c5847205c893b9bd0fb
Mamduh Halawa <mamduhkod@gmail.com>
2024-06-21 20:33:03 +0200

    retried, did not work

--- kbt_app/experiments/urls.py
+++ kbt_app/experiments/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
 from . import views
-from .views import ExperimentPlanView
+from .views import ExperimentPlanView, ExperimentResultatView
 
 urlpatterns = [
     path("", views.Index, name="Index"),
     path("skapa/",ExperimentPlanView.as_view(), name="Skapa"), 
-    path('experiments/<int:experiment_id>/record_result/', views.record_result, name='record_result'),   
-    
+    path('<int:experiment_id>/record_result/', ExperimentResultatView.as_view(), name='Klart_experiment'),
+    path('<int:experiment_id>/record_result/result', views.Resultat, name='Resultat'),    
     
 ]
--- kbt_app/experiments/views.py
+++ kbt_app/experiments/views.py
@@ -2,34 +2,19 @@ from .models import ExperimentPlan, ExperimentResultat
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect
-from .forms import ExperimentResultForm
+
 
 # Create your views here.
-
 
 def Index(request):
     experiments = ExperimentPlan.objects.all()  
     context = {'experiments': experiments}  
-    
-    if request.method == "POST":
-     experiment_id = request.POST.get('experiment_id')
-     experiment = ExperimentPlan.objects.get(pk=experiment_id)
-     form = ExperimentResultForm(request.POST)
-     
-     if form.is_valid():
-         result = form.save(commit=False)  
-         result.Experiment = experiment  
-         result.tro_pre_stored = experiment.tro_pre 
-         result.save()  
-         return redirect('index')
-    
-    
     return render(request, 'experiments/index.html', context)
 
 class ExperimentPlanView(CreateView):
     model = ExperimentPlan
     fields = ["negativ_tanke","tro_pre","datum","Beteende","Situation","Konsekvens"]
-    success_url = reverse_lazy(('Index'))
+    success_url = reverse_lazy(('Klart_experiment'))
     
     def get(self, request, *args, **kwargs):
      context = {} 
@@ -37,19 +22,18 @@ class ExperimentPlanView(CreateView):
      return render(request, 'experiments/tanke_form.html', context)
 
 
-def record_result(request, experiment_id):
-    if request.method == 'POST':
-        form = ExperimentResultForm(request.POST)
-        if form.is_valid():
-            # Save the form data (process results)
-            # ... (your logic to save results)
-            return redirect('experiments:index')  # Redirect to experiments list after saving
-    else:
-        form = ExperimentResultForm()
-    context = {'form': form, 'experiment_id': experiment_id}
-    return render(request, 'experiments/record_result.html', context)
-
+class ExperimentResultatView(CreateView):
+    model = ExperimentResultat
+    fields = ["resultat","insikter","tro_post"]
+    success_url = reverse_lazy(('Index'))
+    
+    def get(self, request, *args, **kwargs):
+     context = {} 
+     context['form'] = self.get_form()
+     return render(request, 'experiments/record_result.html', context)
     
     
-    
-    
+def Resultat(request,resultat_id ):
+    resultat = ExperimentResultat.objects.get(pk=resultat_id)  
+    context = {'resultat': resultat}  
+    return render(request, 'experiments/result.html', context)
